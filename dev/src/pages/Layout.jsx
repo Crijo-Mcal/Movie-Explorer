240aa0010bcc65858135e3aada4ea87af1a63954
Crijolise Morglia <crijolisemor25@gmail.com>
2026-03-27 01:09:22 +0000

    fix bug

--- dev/src/pages/Layout.jsx
+++ dev/src/pages/Layout.jsx
@@ -1,12 +1,12 @@
 import { Outlet } from "react-router-dom";
-import Navbar from "../components/Navbar";
+import NavBar from "../components/Navbar";
 import Footer from "../components/Footer";
 
 export default function Layout() {
   return (
     <>
       <header className="sticky top-0 w-full h-auto z-10">
-        <Navbar />
+        <NavBar />
       </header>
       <Outlet />
       <Footer />
